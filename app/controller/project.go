2348e559135bab3b76fca7955400729c3a114cc0
choisungwook <kgg1959@naver.com>
2022-04-06 21:40:08 +0900

    go 프로파일 분리 #5

--- app/controller/project.go
+++ app/controller/project.go
@@ -4,12 +4,19 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+
+	"jcp-backend/utils"
 )
 
+var (
+	conf *utils.Config
+)
+
+func init() {
+	conf = utils.LoadConfig()
+}
+
+// 프로젝트 생성
 func CreateProject(c echo.Context) error {
-	/*
-		프로젝트 생성
-	*/
-	example_data := []string{"hello"}
-	return c.JSON(http.StatusOK, example_data)
+	return c.JSON(http.StatusOK, conf.Sample)
 }
